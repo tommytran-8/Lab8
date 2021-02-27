ecca5ebbf43a0d35580938f29d55d63be3d7f841
Tommy Tran <totran@ucsd.edu>
2021-02-26 23:57:38 -0800

    Update custom-tests.js

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -1,6 +1,6 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress/index.html');
   });
 
   it('First Test', () => {
